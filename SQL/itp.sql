b9c1a0559cf32537a2cfcb8130c7cf0a4375d834
KGMalith <IT18258622@my.sliit.lk>
2020-05-10 19:19:01 +0530

    Updated SQL

--- SQL/itp.sql
+++ SQL/itp.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 10, 2020 at 03:04 PM
+-- Generation Time: May 10, 2020 at 03:47 PM
 -- Server version: 10.1.30-MariaDB
 -- PHP Version: 7.2.2
 
